880b9f48c3e4899ba0a241419b5229e4c21c4745
Dimple <dimple.wanigatunga@gmail.com>
2021-06-26 17:39:20 +1000

    initial commit

---
+++ src/main/java/quote/car/App.java
@@ -0,0 +1,13 @@
+package quote.car;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
